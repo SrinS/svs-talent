9d82c8782c53566a67ca44e349dc7e54131e7f7c
nikola.zivkov <nikola.zivkov@seavus.com>
2015-03-18 14:39:40 +0100

    Revert "Chapter 13 updated."

---
+++ 01. Java SE/Code/13.06 - Exercise - WaterHeater/WaterHeater.Start/src/com/ventoelectrics/waterheater/StandardVentoWaterHeaterApp.java
@@ -0,0 +1,18 @@
+package com.ventoelectrics.waterheater;
+
+public class StandardVentoWaterHeaterApp {
+
+	public static void main(String[] args) throws Exception {
+
+		final VentoThermometer ventoThermometer = new VentoThermometer();
+		final VentoHeater ventoHeater = new VentoHeater();
+		final VentoPowerSwitch ventoPowerSwitch = new VentoPowerSwitch();
+
+		final VentoThermoregulator ventoThermoregulator = null; // ACME standard thermoregulator instance. 
+
+		ventoPowerSwitch.controlPowerFor(ventoThermoregulator);
+		ventoPowerSwitch.controlPowerFor(ventoHeater);
+
+		VentoWaterHeaterApp.run(ventoThermoregulator, ventoPowerSwitch);
+	}
+}
